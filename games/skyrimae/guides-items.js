b1602c3d11ae01a01c38cd42a668890007fe10ff
Theowulf-dev <DioKyrie@proton.me>
2025-06-09 10:09:18 +0300

    Added new element to guides-items.js

--- games/skyrimae/guides-items.js
+++ games/skyrimae/guides-items.js
@@ -6,12 +6,20 @@ const categories = {
 const contentItems = {
     appsandtools: [
         {
-      id: "creation-kit-install",
-      title: "Creation Kit",
-      link: "items/creation-kit-install.html",
-      description: "Install Creation Kit via Steam.",
-      image: "items/creation-kit-install_res/ck_image.png",
-      tags: ["Creation Kit", "Tools", "Apps", "Steam"]
+             id: "creation-kit-install",
+           title: "Creation Kit",
+           link: "items/creation-kit-install.html",
+           description: "Install Creation Kit via Steam.",
+           image: "items/creation-kit-install_res/ck_image.png",
+           tags: ["Creation Kit", "Tools", "Apps", "Steam"]
+        },
+        {
+            id: "creation-kit-platform-extended",
+            title: "Creation Kit Platform Extended",
+            link: "items/creation-kit-platform-extended.html",
+            description: "Tool to provide extended functionality for the Creation Kit, such as a more user-friendly interface and additional features.",
+            image: "items/creation-kit-platform-extended_res/ckpe_image.png",
+            tags: ["Creation Kit", "Tools", "Apps", "Creation Kit Platform Extended"]
         },
         //add more items here...
   ]
